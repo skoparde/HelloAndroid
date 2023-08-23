5718029b3d86b39da8c4eadd60d6f18d3a2d7f33
skoparde <skoparde@yahoo.com>
2023-08-22 22:11:24 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HelloAndroidTheme {
-        Greeting("Android")
+        Greeting("Suchit")
     }
 }
